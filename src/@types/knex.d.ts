93c3553ba516d531b3a7776d245b394a5c91905e
Rute Figueiredo <rute.figueiredo@radically.digital>
2023-05-25 11:56:31 +0100

    declared extended typed Tables for knex with meal table type

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,13 @@
+import 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    meals: {
+      id: string
+      name: string
+      description?: string
+      created_at: string
+      is_following_diet: boolean
+    }
+  }
+}
